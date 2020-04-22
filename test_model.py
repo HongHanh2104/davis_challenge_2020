2c29d961dec7fde2b2b14ef71fe3af53dce1eaca
vltanh <vltheanh030698@gmail.com>
2020-04-22 08:44:53 +0700

    training pipeline

--- test_model.py
+++ test_model.py
@@ -1,63 +1,62 @@
-import torch 
-from models.canet import CANet
-from models.resnet import ResNetExtractor
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
 from PIL import Image
 import numpy as np
 from torchvision import transforms as tvtf
+from tqdm import tqdm
 
-class NormMaxMin():
-    def __call__(self, x):
-        return (x.float() - torch.min(x)) / (torch.max(x) - torch.min(x))
+from models.canet_org import Res_Deeplab
+from models.canet import CANet
+from models.resnet import ResNetExtractor
+from datasets.davis import DAVISDataset
+from utils.device import move_to
 
-def getImage(support_path, annotation_path, query_path):
-    support_img = Image.open(support_path).convert('RGB')
-    support_arr = np.array(support_img)
-    support_img_tf = tvtf.Compose([
-        NormMaxMin()
-    ])  #np.array(support_name)
-    support_img = support_img_tf(torch.Tensor(support_arr)).unsqueeze(0).permute(0, 3, 1, 2)
-    
-    query_img = Image.open(query_path).convert('RGB')
-    query_arr = np.array(query_img)
-    query_img_tf = tvtf.Compose([
-        NormMaxMin()
-    ])
-    query_img = query_img_tf(torch.Tensor(query_arr)).unsqueeze(0).permute(0, 3, 1, 2)
-    
-    anno_img = Image.open(annotation_path).convert("L")
-    anno_arr = np.array(anno_img)
-    anno_img_tf = tvtf.Compose([
-    ])
-    anno_img = torch.Tensor(anno_img_tf(anno_arr)).unsqueeze(0).unsqueeze(0)
+import argparse
+import matplotlib.pyplot as plt
 
-    return support_img, anno_img, query_img    
 
-def test(support_path, anno_path, query_path):
-    '''
-    query = torch.rand(1,3,854,480).cpu()
-    support = torch.rand(1,3,854,480).cpu()
-    annotation = torch.rand(1,1,854,480).cpu()
-    '''
-    support_img, anno_img, query_img = getImage(support_path, anno_path, query_path)
-    print(support_img.shape, anno_img.shape, query_img.shape)
-    extractor = ResNetExtractor('resnet50').cpu()
-    net = CANet(num_class=2, extractor=extractor).cpu()
-    out = net(support=support_img, annotation=anno_img, query=query_img)
+def get_dummy(ds_size, in_channels=3, nclasses=2, h=854, w=480):
+    return [
+        [
+            (
+                torch.rand(in_channels, h, w).cpu(),
+                torch.randint(low=0, high=nclasses, size=(h, w)),
+                torch.rand(in_channels, h, w).cpu()
+            ),
+            torch.randint(low=0, high=nclasses, size=(h, w))
+        ]
+        for _ in range(ds_size)
+    ]
 
-    return torch.argmax(out, dim=1)
+
+def get_dataset(root, mode):
+    return DAVISDataset(root_path=root, mode=mode)
+
 
 if __name__ == "__main__":
-    import argparse
-    import matplotlib.pyplot as plt
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--support")
-    parser.add_argument("--annotation")
-    parser.add_argument("--query")
-    args = parser.parse_args()
-    pred = test(args.support, args.annotation, args.query)
-    #pred = pred.detach()
-    print(pred.shape)
-    plt.imshow(pred.squeeze(0))
-    plt.show()
-    plt.close()
-    
+    dev = torch.device('cuda')
+
+    dataset = get_dummy(ds_size=100, h=100, w=100)
+    dataloader = DataLoader(dataset, batch_size=4, shuffle=True)
+
+    extractor = ResNetExtractor('resnet50').to(dev)
+    # print(extractor)
+    # net = CANet(num_class=2, extractor=extractor).to(dev)
+    net = Res_Deeplab(num_classes=2).to(dev)
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(net.parameters())
+
+    tbar = tqdm(dataloader)
+    for iter_id, (inps, lbls) in enumerate(tbar):
+        inps = move_to(inps, dev)
+        lbls = move_to(lbls, dev)
+
+        outs = net(inps)
+
+        loss = criterion(outs, lbls)
+        loss.backward()
+        optimizer.step()
+
+        tbar.set_description_str(f'{iter_id}: {loss.item()}')
