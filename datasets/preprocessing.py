86a24fb305023b430e9b0e5068b27a596d120d61
HongHanh <honghanh.nguyen2104@gmail.com>
2020-06-15 10:21:02 +0700

    fix preprocessing.py

--- datasets/preprocessing.py
+++ datasets/preprocessing.py
@@ -11,30 +11,51 @@ PASCAL_CLASS = ['aeroplane','bicycle','bird','boat','bottle', \
                 'diningtable','dog','horse','motorbike','person', \
                 'pottedplant','sheep','sofa','train','tvmonitor']
 
-def create_class_folder(root,
-                        seg_class='SegmentationClass',
-                        dst_folder='Fss_Pascal'):
-    # create class folder
-    root = Path(root)
-    [os.system('mkdir -p {}/{}/{}'.format(root / dst_folder, x, y)) \
-        for x in ['JPEGImages', 'Annotations'] for y in PASCAL_CLASS]
+IMAGE_FOLDER = "JPEGImages"
+MASK_FOLDER = "Annotations"
+IMAGE_SETS = "ImageSets"
+SEGCLASS_FOLDER = "SegmentationClass"
+
+def create_data_structure(root,
+                        classes,
+                        dst_folder):
+    #[os.system(f"mkdir -p {root / dst_folder}/JPEGImages/{y}") for y in classes[0:5]]
+    
+    # Create ImageSets folder
+    os.system(f"mkdir -p {root / dst_folder}/{IMAGE_SETS}")
+
+    # Create JPEGImages and Annotations
+    [os.system(f"mkdir -p {root / dst_folder}/{x}/{y}") \
+        for x in [IMAGE_FOLDER, MASK_FOLDER] for y in classes]
+
+def create_class_txt(root,
+                     classes,
+                     dst_folder):
+    
+    namefile = "classes.txt"
+    with open(root / dst_folder / "ImageSets" / namefile, "w") as f:
+        for c in classes:
+            f.write(c + "\n")        
 
 def pascal_preprocess(root,
                       data_folder,
-                      seg_class='SegmentationClass',
-                      img_folder='JPEGImages',
-                      mask_folder='Annotations',
-                      dst_folder='Fss_Pascal'):
-    # Create folder 
-    create_class_folder(root, seg_class, dst_folder)
-
+                      dst_folder='fss_pascal'):
+    
     root = Path(root)
-    root_img_path = root / data_folder / img_folder
-    dst_img_path = root / dst_folder / img_folder
-    dst_mask_path = root / dst_folder / mask_folder
 
-    for _file in os.listdir(root / data_folder / seg_class):  #['2007_000032.png', '2007_000042.png', '2007_000068.png']: 
-        path = root / data_folder / seg_class / _file
+    # Create folder 
+    create_data_structure(root, PASCAL_CLASS, dst_folder)
+
+    # Create class txt file
+    create_class_txt(root, PASCAL_CLASS, dst_folder)
+    
+    root_img_path = root / data_folder / IMAGE_FOLDER
+    segclass_path = root / data_folder / SEGCLASS_FOLDER
+    dst_img_path = root / dst_folder / IMAGE_FOLDER
+    dst_mask_path = root / dst_folder / MASK_FOLDER
+        
+    for _file in os.listdir(segclass_path):  #['2007_000032.png', '2007_000042.png', '2007_000068.png']: 
+        path = segclass_path / _file
         mask = Image.open(path).convert('P')
         class_id = np.unique(mask)
         class_id = [x for x in class_id if 0 < x < 255]
@@ -57,13 +78,14 @@ def pascal_preprocess(root,
             
     
     print("Done")
+    
 
-def visualize(root, img_folder, mask_folder):
+def visualize(root):
     import random
 
     root = Path(root)
-    img_path = root / img_folder
-    mask_path = root / mask_folder
+    img_path = root / IMAGE_FOLDER
+    mask_path = root / MASK_FOLDER
     classes = random.sample(PASCAL_CLASS, 5 + 1)
     for c in classes:
         imgs = random.sample(os.listdir(img_path / c), 3 + 1)
@@ -78,7 +100,16 @@ def visualize(root, img_folder, mask_folder):
             plt.show()
             plt.close()
 
+def fss1000_preprocess(root,
+                      data_folder,
+                      img_folder='JPEGImages',
+                      mask_folder='Annotations',
+                      dst_folder='new_fss1000'):
+    root_path = Path(root)
 
+    classes = os.listdir(root_path / data_folder)
+    print(type(classes[0]), type(PASCAL_CLASS[0]))
+    create_data_structure(root, classes, dst_folder)
 
 if __name__ == "__main__":
     
@@ -86,18 +117,13 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--root', help='Path to dataset')
     parser.add_argument('--data_folder', help='The folder of dataset')
-    parser.add_argument('--seg_class', help='The folder of SegmentationClass',
-                        default='SegmentationClass')
-    parser.add_argument('--img_folder', help='The folder of images.',
-                        default='JPEGImages')
-    parser.add_argument('--mask_folder', help='The folder of masks.',
-                        default='Annotations')
-    parser.add_argument('--dst_folder', help='The destination folder.', \
-                        default='Fss_Pascal')
+    parser.add_argument('--dst_folder', help='The destination folder.')
     args = parser.parse_args()
 
-    #pascal_preprocess(args.root, args.data_folder, args.seg_class, args.img_folder, args.mask_folder, args.dst_folder)
+    #pascal_preprocess(args.root, args.data_folder, args.dst_folder)
     
-    visualize(os.path.join(args.root, args.dst_folder), args.img_folder, args.mask_folder)
+    visualize(os.path.join(args.root, args.dst_folder))
+
+    #fss1000_preprocess(args.root, args.data_folder, args.img_folder, args.mask_folder, args.dst_folder)
 
 
