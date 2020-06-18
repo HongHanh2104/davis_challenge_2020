dc4209cc4adfdb4cb0b45e793b08c1e18c00c924
vltanh <vltheanh030698@gmail.com>
2020-06-18 22:37:33 +0700

    fix template: add another version of mIoU which is calculated as sum(intersection) / sum(union) instead of mean(intersection / union)

--- metrics/segmentation/mean_iou.py
+++ metrics/segmentation/mean_iou.py
@@ -6,7 +6,53 @@ from utils.segmentation import multi_class_prediction, binary_prediction
 
 
 class MeanIoU():
-    def __init__(self, nclasses, ignore_index=None, eps=1e-6):
+    def __init__(self, nclasses, ignore_index=None, eps=1e-9):
+        super().__init__()
+        assert nclasses > 0
+
+        self.nclasses = nclasses
+        self.ignore_index = ignore_index
+        self.eps = eps
+        self.reset()
+
+    def calculate(self, output, target):
+        nclasses = output.size(1)
+        prediction = torch.argmax(output, dim=1)
+        prediction = F.one_hot(prediction, nclasses).bool()
+        target = F.one_hot(target, nclasses).bool()
+        intersection = (prediction & target).sum((-3, -2))
+        union = (prediction | target).sum((-3, -2))
+        return intersection.cpu(), union.cpu()
+
+    def update(self, value):
+        self.intersection += value[0].sum(0)
+        self.union += value[1].sum(0)
+        self.sample_size += value[0].size(0)
+
+    def value(self):
+        ious = (self.intersection + self.eps) / (self.union + self.eps)
+        miou = ious.sum()
+        nclasses = ious.size(0)
+        if self.ignore_index is not None:
+            miou -= ious[self.ignore_index]
+            nclasses -= 1
+        return miou / nclasses
+
+    def reset(self):
+        self.intersection = torch.zeros(self.nclasses).float()
+        self.union = torch.zeros(self.nclasses).float()
+        self.sample_size = 0
+
+    def summary(self):
+        class_iou = (self.intersection + self.eps) / (self.union + self.eps)
+
+        print(f'mIoU: {self.value():.6f}')
+        for i, x in enumerate(class_iou):
+            print(f'\tClass {i:3d}: {x:.6f}')
+
+
+class _MeanIoU():
+    def __init__(self, nclasses, ignore_index=None, eps=1e-9):
         super().__init__()
         assert nclasses > 0
 
